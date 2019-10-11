cddaf58b080521bd00a8bc4d8b8109294f1e8224
Mike Vartanian <mike.vartanian@gmail.com>
2019-10-11 12:48:12 -0400

    Updated button stylings. Starting to tput shape to navigation... User can choose the game type or adjust settings... Maybe we also need a quick play game button that plays the selected game?

--- Home.js
+++ Home.js
@@ -12,7 +12,7 @@ import { StaveNote } from 'vexflow/src/stavenote';
 import { Voice } from 'vexflow/src/voice';
 import { Formatter } from 'vexflow/src/formatter';
 import { ReactNativeSVGContext, NotoFontPack } from 'standalone-vexflow-context';
-import { Dimensions } from 'react-native';
+import { Dimensions, TouchableOpacity } from 'react-native';
 
 import { Button } from 'react-native-elements';
 import { VexFlow } from './VexUtility.js';
@@ -46,9 +46,14 @@ export default class Home extends Component {
           Music Note Game
         </Text>
         <VexFlow musicObject = {musicObjectData} style={styles.scaler}/>
-        <Button
-          title="Button" onPress={() => navigate('Game', {name: 'Jane'})}
-        />
+        <View style={styles.buttonGroupStyle}>
+          <TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {name: 'Jane'})}>
+            <Text style={styles.buttonTextStyle}>Choose Game</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {name: 'Jane'})}>
+            <Text style={styles.buttonTextStyle}>Settings</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -71,6 +76,31 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  buttonGroupStyle: {
+    flex: 1,
+    width: "80%",
+  },
+  buttonStyles: {
+    justifyContent: 'center',
+    borderRadius: 5,
+    height: 50,
+    
+    margin: 5,
+    backgroundColor: "red",
+    shadowColor: "#000",
+    shadowOpacity: 0.25,
+    shadowRadius: 3.8,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
+  buttonTextStyle: {
+    fontFamily: "GoodDog Plain",
+    fontSize: 30,
+    textAlign: 'center',
+    color: "white",
   },
   scaler: {
     transform: [{scaleX: 2.5}, {scaleY: 4.0}],
