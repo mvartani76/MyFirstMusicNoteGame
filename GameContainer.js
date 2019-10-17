7d11ff4ba769f032e31105f97c3c1d1a230f6f9b
Mike Vartanian <mike.vartanian@gmail.com>
2019-10-17 16:14:55 -0400

    Reformatted to 4 space tabs.

--- GameContainer.js
+++ GameContainer.js
@@ -18,16 +18,16 @@ import { Button } from 'react-native-elements';
 import { VexFlow } from './VexUtility.js';
 
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView
 } from 'react-native';
 
 export default class GameContainer extends Component {
-  mode = JSON.stringify(this.props.navigation.getParam('mode','game')).replace(/\"/g, "");
-	constructor(props) {
+	mode = JSON.stringify(this.props.navigation.getParam('mode','game')).replace(/\"/g, "");
+		constructor(props) {
 		super(props);
 
 		const screenWidth = Dimensions.get('window').width;
@@ -46,51 +46,51 @@ export default class GameContainer extends Component {
 		}
 	}
 
-  render() {
-    const {navigate} = this.props.navigation;
+	render() {
+	    const {navigate} = this.props.navigation;
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.topContainer}>
-          <View style={styles.topTextView}>
-            <Text style={styles.welcome}>
-              Music Note {this.mode}
-            </Text>
-          </View>
-          {/* In order to prevent scaling overlapping other divs/components, need to have two parent views
-            surrounding the Vexflow Component. */}
-          <VexFlow
-            musicObject = {this.state.musicObjectData}
-            outerStyle={styles.outerVexFlowContainer}
-            innerStyle={styles.innerVexFlowContainer}
-            style={styles.scaler}/>
-        </View>
-        <View style={styles.buttonGroupStyle}>
-          <TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {clef: 'treble', mode: this.mode})}>
-            <Text style={styles.buttonTextStyle}>Treble Notes</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {clef: 'bass', mode: this.mode})}>
-            <Text style={styles.buttonTextStyle}>Bass Notes</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {clef: 'bass', mode: this.mode})}>
-            <Text style={styles.buttonTextStyle}>Rhythms</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {clef: 'bass', mode: this.mode})}>
-            <Text style={styles.buttonTextStyle}>Symbols</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    );
-  }
+		return (
+			<SafeAreaView style={styles.container}>
+				<View style={styles.topContainer}>
+					<View style={styles.topTextView}>
+						<Text style={styles.welcome}>
+							Music Note {this.mode}
+						</Text>
+					</View>
+					{/* In order to prevent scaling overlapping other divs/components, need to have two parent views
+					    surrounding the Vexflow Component. */}
+					<VexFlow
+						musicObject = {this.state.musicObjectData}
+						outerStyle={styles.outerVexFlowContainer}
+						innerStyle={styles.innerVexFlowContainer}
+						style={styles.scaler}/>
+				</View>
+				<View style={styles.buttonGroupStyle}>
+					<TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {clef: 'treble', mode: this.mode})}>
+						<Text style={styles.buttonTextStyle}>Treble Notes</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {clef: 'bass', mode: this.mode})}>
+						<Text style={styles.buttonTextStyle}>Bass Notes</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {clef: 'bass', mode: this.mode})}>
+						<Text style={styles.buttonTextStyle}>Rhythms</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.buttonStyles} onPress={() => navigate('Game', {clef: 'bass', mode: this.mode})}>
+						<Text style={styles.buttonTextStyle}>Symbols</Text>
+					</TouchableOpacity>
+				</View>
+			</SafeAreaView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
   	topContainer: {
 		flex: 0.7,
 		flexDirection: 'column',
@@ -98,61 +98,60 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		backgroundColor: 'red',
 	},
-  welcome: {
-    fontFamily: "GoodDog Plain",
-    fontSize: 50,
-    textAlign: 'center',
-    margin: 10,
-  },
-  buttonGroupStyle: {
-    flex: 0.3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: "80%",
-    backgroundColor: 'pink',
-  },
-  buttonStyles: {
-    flex: 2,
-    justifyContent: 'center',
-    borderRadius: 5,
-    //height: 50,
-    alignSelf: 'stretch',
-    margin: 5,
-    backgroundColor: "red",
-    shadowColor: "#000",
-    shadowOpacity: 0.25,
-    shadowRadius: 3.8,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-  },
-  buttonTextStyle: {
-    fontFamily: "GoodDog Plain",
-    fontSize: 30,
-    textAlign: 'center',
-    color: "white",
-  },
+	welcome: {
+		fontFamily: "GoodDog Plain",
+		fontSize: 50,
+		textAlign: 'center',
+		margin: 10,
+	},
+	buttonGroupStyle: {
+		flex: 0.3,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: "80%",
+		backgroundColor: 'pink',
+	},
+	buttonStyles: {
+		flex: 2,
+		justifyContent: 'center',
+		borderRadius: 5,
+		alignSelf: 'stretch',
+		margin: 5,
+		backgroundColor: "red",
+		shadowColor: "#000",
+		shadowOpacity: 0.25,
+		shadowRadius: 3.8,
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+	},
+	buttonTextStyle: {
+		fontFamily: "GoodDog Plain",
+		fontSize: 30,
+		textAlign: 'center',
+		color: "white",
+	},
   	topTextView: {
 		flex: 0.2,
 		width: "100%",
 		backgroundColor: "aliceblue",
 	},
-  innerVexFlowContainer: {
-    height: "80%",
-    backgroundColor: "darkcyan",
-    justifyContent: "center",
-    overflow: "hidden",
-  },
-  outerVexFlowContainer: {
-    overflow: "hidden",
-    justifyContent: "center",
-    flex: 0.7,
-    backgroundColor: "darkblue",
-  },
-  scaler: {
-    overflow: "hidden",
-    backgroundColor: 'purple',
-    transform: [{scaleX: 2.5}, {scaleY: 4.0}, {translateX: "0%"}, {translateY: "17%"}],
-  },
+	innerVexFlowContainer: {
+		height: "80%",
+		backgroundColor: "darkcyan",
+		justifyContent: "center",
+		overflow: "hidden",
+	},
+	outerVexFlowContainer: {
+		overflow: "hidden",
+		justifyContent: "center",
+		flex: 0.7,
+		backgroundColor: "darkblue",
+	},
+	scaler: {
+		overflow: "hidden",
+		backgroundColor: 'purple',
+		transform: [{scaleX: 2.5}, {scaleY: 4.0}, {translateX: "0%"}, {translateY: "17%"}],
+	},
 });
